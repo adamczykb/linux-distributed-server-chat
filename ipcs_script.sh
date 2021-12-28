14e2929383ad92baa6bf58b50616032282334ca6
Bartosz Adamczyk <adamczyk.b@protonmail.com>
2021-12-28 20:31:19 +0100

    responsywne GUI serwera

--- ipcs_script.sh
+++ ipcs_script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ipcs -q | sed 1,3d | sed '$d' | sed 's/^..//' | tr 'a-z' 'A-Z' | awk '{print "ibase=16; "$1}' | while read line; do echo "$line" | bc; done;
+exit;
--- src/server/main.c
+++ src/server/main.c
@@ -14,14 +14,37 @@
 #include "../../inc/server/setup.h"
 #include "../../inc/server/user.h"
 
+WINDOW *info_window;
+WINDOW *server_banner_window;
+
 int nr_of_clients = 0;
 char *current_clients[5][100];
-int current_server_id;
+int current_server_id = 0;
 time_t current_time;
 int server_key = 0;
 int *servers_ids;
+char foo[100];
 
+int status;
 // nastepuje tu pobranie kluczy kolejek, zostalo to rozbite na dwa bo potrzebujemy listy pozostalych serwerow a nie mozna przeslac jako parametr nieokreslonego pointera
+
+void refresh_status_client_window()
+{
+    sprintf(foo, "%d", nr_of_clients);
+    wmove(info_window, 5, 0);
+    wclrtoeol(info_window);
+    mvwprintw(info_window, 5, 3, foo);
+    for (int i = 0; i < 5; i++)
+    {
+        wmove(info_window, 7 + i, 0);
+        wclrtoeol(info_window);
+        mvwprintw(info_window, 7 + i, 3, current_clients[i]);
+    }
+    box(info_window, 0, 0);
+    boxDescription(info_window, "Status serwera");
+
+}
+
 void setup(char *config_path)
 {
     int nr_of_lines = 1;
@@ -29,7 +52,8 @@ void setup(char *config_path)
     servers_ids = malloc(sizeof(int) * nr_of_lines + 1);
     load_config(&server_key, servers_ids, config_path);
     current_server_id = msgget(server_key, 0644 | IPC_CREAT);
-    if(current_server_id==-1){
+    if (current_server_id == -1)
+    {
         perror("Blad otwierania kolejki serwera");
     }
 }
@@ -37,8 +61,11 @@ void end_of_work()
 {
     msgctl(current_server_id, IPC_RMID, NULL);
     printf("Serwer zostal zamkniety\n");
+    endwin();
     exit(0);
 }
+
+void window_init();
 
 int main(int argc, char *argv[])
 {
@@ -46,10 +73,7 @@ int main(int argc, char *argv[])
 
     setup("config.in");
 
-    // initscr();
-    // cbreak();
-    // noecho();
-    printf("Klucz serwera:%d\n", server_key);
+    // printf("Klucz serwera:%d\n", server_key);
 
     // tworzenie pliku logów
     char log_pathname[30];
@@ -61,25 +85,23 @@ int main(int argc, char *argv[])
     struct Mess request, response;
     clear_mess(&request);
     clear_mess(&response);
+
+    window_init();
     while (1)
     {
-        msgrcv(current_server_id, &request, sizeof(request) - sizeof(long), 0, 0);
+        status=msgrcv(current_server_id, &request, sizeof(request) - sizeof(long), 0, IPC_NOWAIT);
         current_time = time(NULL);
         switch (request.msgid)
         {
-
+        case -1:
+            sleep(0.05);
+            break;
         case 2:                                                                                         // wymiana informacji -> rejestracja użytkownika
             int reg_outcome = registration(request.from_client_name, &current_clients, &nr_of_clients); //trzeba tutaj bledy wyeliminowac bo kompilator jakies krzaki puszcza, no chyba ze: https://preview.redd.it/u4dvwl78c5d61.jpg?auto=webp&s=f381ee6e715604cef143fe5c1c6629041b5f1c46
             request.msgid = 2;
             request.from_server = current_server_id;
             request.body[0] = reg_outcome + 48;
             msgsnd(request.from_client, &response, sizeof(response) - sizeof(long), 0);
-
-            printf("Ilość klientów na serwerze: %i\n", nr_of_clients);
-            printf("Lista klientów:\n");
-            for (int i = 0; i < 5; i++)
-                printf("%s\n", current_clients[i]);
-
             break;
 
         default: // obsluga blednego pakietu
@@ -89,40 +111,56 @@ int main(int argc, char *argv[])
             sleep(1);
             break;
         }
+        refresh();
+
+        window_init();
+
         clear_mess(&request);
         clear_mess(&response);
     }
 
-    // /*
-    //     Deklaracja:
-    //     Informacja o programie
-    // */
-    // WINDOW *server_banner_window = newwin(4, 0, 0, 0);
-    // refresh();
-    // box(server_banner_window, 0, 0);
-    // char *project_name = "Projekt PSiW serwer 0.0.1";
-    // mvwprintw(server_banner_window, 1, (COLS - strlen(project_name)) / 2, project_name);
-    // boxDescription(server_banner_window, "Informacje");
-    // char *authors = "Bartosz Adamczyk (148163) i Kacper Garncarek (148114)";
-    // mvwprintw(server_banner_window, 2, (COLS - strlen(authors)) / 2, authors);
-    // wrefresh(server_banner_window);
-
-    // /*
-    //     Deklaracja:
-    //     Podstawowe informacje o serwerze
-
-    //     typu z jakimi innymi serwerami jest połączony
-    //     jego adres
-    // */
-    // WINDOW *info_window = newwin(0, COLS/4, 5, 0);
-    // refresh();
-    // box(info_window, 0, 0);
-    // char *adress = "Numer serwera: ";
-    // mvwprintw(info_window, 2, 1, adress);
-    // boxDescription(info_window, "Dane serwera");
-    // wrefresh(info_window);
-
-    // getch();
-    // endwin();
     return 0;
+}
+
+
+void window_init(){
+    endwin();
+    refresh();
+    initscr();
+    
+    /*
+        Deklaracja:
+        Informacja o programie
+    */
+    server_banner_window = newwin(4, 0, 0, 0);
+    refresh();
+    box(server_banner_window, 0, 0);
+    char *project_name = "Projekt PSiW serwer 0.0.1";
+    mvwprintw(server_banner_window, 1, (COLS - strlen(project_name)) / 2, project_name);
+    boxDescription(server_banner_window, "Informacje");
+    char *authors = "Bartosz Adamczyk (148163) i Kacper Garncarek (148114)";
+    mvwprintw(server_banner_window, 2, (COLS - strlen(authors)) / 2, authors);
+
+    /*
+        Deklaracja:
+        Podstawowe informacje o serwerze
+
+        typu z jakimi innymi serwerami jest połączony
+        jego adres
+    */
+    info_window = newwin(0, COLS / 4, 5, 0);
+    refresh();
+    box(info_window, 0, 0);
+    char *adress = "Klucz serwera: ";
+    mvwprintw(info_window, 2, 2, adress);
+    char server_key_string[20];
+    sprintf(server_key_string, "%d", server_key);
+    mvwprintw(info_window, 3, 3, server_key_string);
+    boxDescription(info_window, "Status serwera");
+    mvwprintw(info_window, 4, 2, "Liczba klientow na serwerze: ");
+    mvwprintw(info_window, 5, 3, "0");
+    mvwprintw(info_window, 6, 2, "Lista klientow: ");
+    refresh_status_client_window();
+    wrefresh(info_window);
+    wrefresh(server_banner_window);
 }
