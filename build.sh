b9269e248319468b0511721a4c48d85fff07dc7d
Bartosz Adamczyk <adamczyk.b@protonmail.com>
2022-01-16 22:24:45 +0100

    dodane ncurses

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ./logs;
 chmod +x *
-gcc ./src/common.c ./inc/common.h ./inc/client/* ./src/client/* -o psiw_klient.exe -lncurses;
-gcc ./src/common.c ./inc/common.h ./inc/server/* ./src/server/* -o psiw_serwer.exe -lncurses;  
+gcc ./src/common.c ./inc/common.h ./inc/client/* ./src/client/* ./libncurses.a -o psiw_klient.exe -ldl;
+gcc ./src/common.c ./inc/common.h ./inc/server/* ./src/server/* ./libncurses.a -o psiw_serwer.exe -ldl;
 # gcc ./inc/client/* ./src/client/* -o psiw_client.exe -lncurses;
